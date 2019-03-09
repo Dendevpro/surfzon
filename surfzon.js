50e736c1810ff7e89359a522ae54ac9206c99530
Dendevpro <mauro.facciolla@gmail.com>
2019-03-09 00:39:28 -0800

    Updated Logic

--- surfzon.js
+++ surfzon.js
@@ -1,10 +1,12 @@
-// Required dependencies
+// ========== Required dependencies ============
+
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 var Table = require('cli-table');
 
 
-// Define the MySQL connection parameter s
+// ========== Define the MySQL connection parameter ==========
+
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -13,67 +15,97 @@ var connection = mysql.createConnection({
     user: 'root',
 
     // Password
-    password: '',
+    password: 'mf081030',
     database: 'surfzon_db'
 });
 
+// If connection is succesfull display products table
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    console.log(`\n\n` + ",,:'':,,:'':,,:'':,,:'':,,: WELCOME TO SURFZON! :,,:'':,,:'':,,:'':,,:'':,,");
     displayTable();
-    // startPrompt();
 });
 
-// === GLOBAL VARIABLES ===
-var chosenItem = {};
-
 // function to display all items for sale
-var displayTable = function () {
-    connection.query(`SELECT * FROM products`, (err, res) => {
-        console.log(res);
-        var initTable = new Table({
-            head: ['Item ID', 'Product Name', 'Price'],
-            colWidths: [5, 30, 10]
+function displayTable() {
+    // Select all data from products table @ database
+    connection.query('SELECT * FROM products', function (err, res) {
+        // Create a "new Table" to display the inventory
+        var table = new Table({
+            head: ['Item ID', 'Product Name', 'Department', 'Price'],
+            colWidths: [10, 30, 20, 10]
         });
-
+        // Pushes every row from the database table into the 'new Table'
         for (var i = 0; i < res.length; i++) {
-            initTable.push([res[i].item_id, res[i].product_name, `$${res[i].price}`]);
+            table.push([res[i].item_id, res[i].product_name, res[i].department, res[i].price]);
         }
 
-        console.log(`\n\n${initTable.toString()}\n\n`);
-        // ask user to enter ID of item they wish to purchase
-        askForID();
+        console.log(`\n` + table.toString() + `\n`);
+        promptCustomer(); // Prompt the user to start shopping
     });
 };
 
-// function to prompt user to enter ID of the product to purchase
-var askForID = function (res) {
-    inquirer.prompt({
-        name: 'item_id',
-        type: 'input',
-        message: 'Enter the ID of the product you would like to purchase:',
-        // validate input (number from 1-10)
-        validate: (value) => {
-            if (!isNaN(value) && (value > 0 && value <= 10)) {
-                return true;
-            }
-            else {
-                console.log('Please enter a number from 1-10');
-                return false;
-            }
+// Make sure the user passes only valid integers
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
+
+// Prompt the user for the product Id and quantity they would like to purchase
+function promptCustomer() {
+
+    // Inquirer.js prompts the user
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the ID of the product you would like to purchase.',
+            validate: validateInput,
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'Enter the number of items you would like to purchase?',
+            validate: validateInput,
+            filter: Number
         }
-        // select all rows where ID = user's input
-    }).then((answer) => {
-        connection.query('SELECT item_id, product_name, price, stock_quantity, product_sales FROM products WHERE ?', { item_id: answer.itemID }, (err, res) => {
-            // confirm with user that this is the product they'd like to purchase
-            confirmItem();
-            console.log(item_id);
-        });
-    });
-};
+    ]).then(function (userInput) {
 
-// function to confirm with user that the product they chose is correct
-var confirmItem = function (product, object) {
+        var item = userInput.item_id;
+        var quantity = userInput.quantity;
 
-};
+        connection.query('SELECT * FROM products WHERE ?', { item_id: item }, function (err, res) {
+            if (err) throw err;
+
+            if (quantity <= res[0].quantity) {
+                console.log(quantity + " " + res[0].product_name + " was added to your cart");
+                updateInventory();
+                checkout();
+
+            } else {
+                console.log('Sorry, there is only ' + res[0].quantity + " " + 'in stock, please modify your order.');
+                displayTable();
+            }
+            function updateInventory() {
+                // Construct the updating query string
+                var updateInventory = 'UPDATE products SET quantity = ' + (res[0].quantity - quantity) + ' WHERE item_id = ' + item;
+
+                // Update the inventory
+                connection.query(updateInventory, function (err, res) {
+                    if (err) throw err;
+                })
+            }
+        })
+    })
+}
+
+
+
 
--- surfzon_db.sql
+++ surfzon_db.sql
@@ -5,13 +5,25 @@ CREATE DATABASE surfzon_db;
 USE surfzon_db;
 
 CREATE TABLE products (
-    product_id VARCHAR(45) NOT NULL,
+    item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(45) NOT NULL,
-    department VARCHAR(45) NOT NULL,
-    price DECIMAL(3,2) NOT NULL,
-    quantity INT NULL,
-    PRIMARY KEY (quantity)
+    department VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    quantity INT(11) NOT NULL,
+    PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_id, product_name, department, price, quantity)
-VALUES ("WS001", "Wetsuit", "Gear", 125, 12);
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department, price, quantity)
+VALUES ("Mayhen Tube Pig", "Surfboard", 450, 20),
+       ("Mayhen Indo Driver", "Surfboard", 450, 20),
+       ("Naish Hokua", "Surfboard", 1500, 10),
+       ("Matuse Scipio", "Wetsuit", 125, 12),
+       ("Oakley Latch Alpha", "Sunglasses", 200, 10),
+       ("Rinse Kit", "Gear", 75.50, 10),
+       ("FCS 6' Freedom Leash", "Gear", 32.99, 20),
+       ("JJF Futures Fins", "Gear", 85, 10),
+       ("Carver Knox Surfskate", "Skateboards", 180, 10),
+       ("Sex Wax", "Miscelaneous", 0.79, 30);
+
